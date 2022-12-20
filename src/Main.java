0d048b901104c7fe65724ef12f5a118c285d7486
Vasya <vasya.454@gmail.com>
2022-12-20 12:25:20 +0300

    Усовершенствованный bubbleSort, добавил mergeSort с рекурсией

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,76 @@
 public class Main {
     public static void main(String[] args) {
-        int[] toSort = {1,2,10,-2,-3,5,6,14,99,-12,7,-8};
-        MyArrays.printArray(toSort);
-        int[] sorted = bubblesSort(toSort);
-        MyArrays.printArray(sorted);
+        int[] left = {9, -2, 3, 0, 99, 15};
+        int[] right = {-12, 11};
+        int[] toSort = {9, -2, 3, 0, 99, 15, -12, 11, 7, -2, 8};
+        MyArrays.printArray(bubbleSort(toSort));
+        MyArrays.printArray(merge(left,right));
+        MyArrays.printArray(mergeSort(toSort));
     }
-    public static int[] bubblesSort(int [] m) {
-        int i = m.length-1;
+
+    public static int[] bubbleSort(int[] m) {
+        int i = m.length - 1;
         int j;
         int buffer;
-        while (i>0) {
+        boolean flag = true;
+        while (i > 0 && flag) {
             j = 0;
+            flag = false;
             while (j < i) {
                 if (m[j] > m[j + 1]) {
                     buffer = m[j];
                     m[j] = m[j + 1];
                     m[j + 1] = buffer;
+                    flag = true;
                 }
                 j++;
             }
             i--;
         }
         return m;
+    }
+
+    public static int[] mergeSort(int[] m) {
+        if (m.length>1) {
+            int[] l = new int[m.length / 2];
+            int[] r = new int[m.length - m.length / 2];
+            for (int i = 0; i < m.length; i++) {
+                if (i<m.length/2) {
+                    l[i] = m[i];
+                } else {
+                    r[i-m.length/2] = m[i];
+                }
+            }
+            return merge(mergeSort(l), mergeSort(r));
+        } else {
+            return m;
+        }
+    }
+
+    private static int[] merge(int[] mLeft, int[] mRight) {
+        int l = 0, r = 0;
+        int i = 0;
+        int[] merged = new int[mLeft.length + mRight.length];
+        while (l < mLeft.length && r < mRight.length) {
+            if (mLeft[l]>mRight[r]) {
+                merged[i] = mRight[r];
+                r++;
+            } else {
+                merged[i] = mLeft[l];
+                l++;
+            }
+            i++;
+        }
+        while (l< mLeft.length) {
+            merged[i] = mLeft[l];
+            l++;
+            i++;
+        }
+        while (r< mRight.length) {
+            merged[i] = mRight[r];
+            r++;
+            i++;
+        }
+        return merged;
     }
 }
